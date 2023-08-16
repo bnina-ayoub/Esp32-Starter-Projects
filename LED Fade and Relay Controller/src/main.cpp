77d17fa6a61a79a527a03c54d12751129da5c12e
Bnina Ayoub <94785911+bnina-ayoub@users.noreply.github.com>
2023-08-16 23:01:23 +0100

    Updating source code

--- LED Fade and Relay Controller/src/main.cpp
+++ LED Fade and Relay Controller/src/main.cpp
@@ -1,14 +1,48 @@
 #include "esp_log.h"
 #include "arduino.h"
 
+const int ledPin = 2;  //  Pin of LED
+const int relayPin = 3;  //  Pin of Relay
 
-const int ledpin = 12;
-void app_main() {
-    pinMode(ledpin, OUTPUT);
+//const int freq = 5000;
+//const int ledChannel = 0;
+//const int resolution = 8;
+
+
+void setup() {
+  // setting PWM properties
+
+  //ledcSetup(ledChannel, freq, resolution);
+  //ledcAttachPin(ledPin, ledChannel);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+ 
 }
-void loop(){    
-    digitalWrite(ledpin, HIGH);
-    delay(1000);
-    digitalWrite(ledpin, LOW);
-    delay(1000);
+
+void loop() {
+
+ for (int i=0; i < 255; i++){
+   // ledcWrite(ledChannel, i);
+     analogWrite(ledPin, i);  
+      delay(50);
+  if(i == 255) {
+    digitalWrite(relayPin, HIGH);
+    delay(2000);      
+ }
+
+}
+ 
+for (int i=255; i >= 0; i--){
+    //ledcWrite(ledPin, i);
+     analogWrite(ledPin, i);  
+    delay(50);
+    if(i == 0) {
+    digitalWrite(relayPin, LOW);
+    delay(2000);      
+    }
+ 
+ }
+
+
 }
